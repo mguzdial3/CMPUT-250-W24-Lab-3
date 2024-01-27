73290a480b64efa3dc11380e7095fe8ae0cc90c6
ChilliChai <khealy@ualberta.ca>
2024-01-27 01:41:55 -0700

    git add -A

--- CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
+++ CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
@@ -4,61 +4,98 @@ using UnityEngine;
 
 public class DVDLogo : MonoBehaviour
 {
-    //Speed it moves at
-    public float speed = 3;
+    // Speed it moves at
+    public float speed = 5;
 
-    //Bounds of the screen (could get these with camera bounds but we can do this since it's a fixed camera)
+    // Bounds of the screen
     public float X_Max = 5, Y_Max = 4;
 
-    //Current direction
+    // Current direction
     private Vector3 direction;
+
+    // Scale factor
+    public float scaleSpeed = 0.1f;
+    private bool isGrowing = true;
+    public float maxScale = 2.0f;
+    public float minScale = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Randomly initialize direction
-        direction = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f));
-        direction.Normalize();
-
-    }
-
-    private void FlipDirectionX(){
-        direction.x*=-1;
-        direction.x+= Random.Range(-0.1f,0.1f);
-        direction.y+= Random.Range(-0.1f,0.1f);
+        // Randomly initialize direction
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         direction.Normalize();
     }
 
-    private void FlipDirectionY(){
-        direction.y*=-1;
-        direction.x+= Random.Range(-0.1f,0.1f);
-        direction.y+= Random.Range(-0.1f,0.1f);
+    private void FlipDirectionX()
+    {
+        direction.x *= -1;
+        direction.x += Random.Range(-0.1f, 0.1f);
+        direction.y += Random.Range(-0.1f, 0.1f);
+        direction.Normalize();
+    }
+
+    private void FlipDirectionY()
+    {
+        direction.y *= -1;
+        direction.x += Random.Range(-0.1f, 0.1f);
+        direction.y += Random.Range(-0.1f, 0.1f);
         direction.Normalize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Move in direction unless we'd go out of bounds, if so bounce with some randomness
+        // Move in direction unless we'd go out of bounds, if so bounce with some randomness
+        Vector3 newPosition = transform.position + direction * Time.deltaTime * speed;
 
-        Vector3 newPosition = transform.position + direction*Time.deltaTime*speed;
-
-        //See if a bounce needs to happen before moving
-        if (newPosition.x>X_Max){
-            FlipDirectionX();
-            
-        }
-        else if (newPosition.x<-1*X_Max){
+        // See if a bounce needs to happen before moving
+        if (newPosition.x > X_Max || newPosition.x < -X_Max)
+        {
             FlipDirectionX();
         }
 
-        if (newPosition.y>Y_Max){
-            FlipDirectionY();
-        }
-        else if (newPosition.y<-1*Y_Max){
+        if (newPosition.y > Y_Max || newPosition.y < -Y_Max)
+        {
             FlipDirectionY();
         }
 
-        transform.position += direction*Time.deltaTime*speed;
+        // Update the scale
+        UpdateScale();
+
+        // Update the position
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    void UpdateScale()
+    {
+        float newScale = transform.localScale.x;
+
+        if (isGrowing)
+        {
+            newScale += scaleSpeed * Time.deltaTime;
+            if (newScale >= maxScale)
+            {
+                newScale = maxScale;
+                isGrowing = false;
+            }
+        }
+        else
+        {
+            newScale -= scaleSpeed * Time.deltaTime;
+            if (newScale <= minScale)
+            {
+                newScale = minScale;
+                isGrowing = true;
+            }
+        }
+
+        transform.localScale = new Vector3(newScale, newScale, 1.0f);
     }
 }
+
+
+
+
+
+
